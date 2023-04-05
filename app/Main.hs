8a4a4e44df1b1c14f427538717bc14ca4de4cc9b
EwenBriand <ewen.briand@epitech.eu>
2023-04-05 12:19:01 +0200

    [ADD] good for little things

--- app/Main.hs
+++ app/Main.hs
@@ -6,71 +6,92 @@
 -}
 
 module Main (main) where
-import Lib ()
-import System.Environment (getArgs)
+
 import Data.Maybe (fromMaybe)
+import Lib
+import System.Environment (getArgs)
 import Text.Read (readMaybe)
+
 -- import Text.Printf (PrintfArg(parseFormat))
 
-data Point = Point
-    { r :: Int
-    , g :: Int
-    , b :: Int
-    } deriving (Show)
-
 data Options = Options
-    { file :: String
-    , cluster :: [Point] -- centroid
-    , convergence :: Maybe Float
-    , nbrColors :: Maybe Int
-    } deriving (Show)
+  { file :: String,
+    cluster :: [Point], -- centroid
+    convergence :: Maybe Float,
+    nbrColors :: Maybe Int
+  }
+  deriving (Show)
 
 parseOptions :: [String] -> Maybe Options
-parseOptions [] = Just Options
-    { file = ""
-    , cluster = []
-    , convergence = Nothing
-    , nbrColors = Nothing
-    }
-
-parseOptions ("-n" : n : xs) = parseOptions xs >>= do
+parseOptions [] =
+  Just
+    Options
+      { file = "",
+        cluster = [],
+        convergence = Nothing,
+        nbrColors = Nothing
+      }
+parseOptions ("-n" : n : xs) =
+  parseOptions xs >>= do
     let n' = readMaybe n :: Maybe Int
     let n'' = fromMaybe 1 n'
-    (\o -> Just o { nbrColors = Just n'' })
-
-parseOptions ("-l" : c : xs) = parseOptions xs >>=  do
+    (\o -> Just o {nbrColors = Just n''})
+parseOptions ("-l" : c : xs) =
+  parseOptions xs >>= do
     let c' = readMaybe c :: Maybe Float
     let c'' = fromMaybe 1 c'
-    (\o -> Just o { convergence = Just c'' })
-
-parseOptions ("-f" : f : xs) = parseOptions xs >>= do
-    (\o -> Just o { file = f })
-
+    (\o -> Just o {convergence = Just c''})
+parseOptions ("-f" : f : xs) =
+  parseOptions xs >>= do
+    (\o -> Just o {file = f})
 parseOptions _ = Nothing
+
+maybeToInt :: Maybe Int -> Int
+maybeToInt Nothing = 0
+maybeToInt (Just x) = x
+
+maybeToFloat :: Maybe Float -> Float
+maybeToFloat Nothing = 0
+maybeToFloat (Just x) = x
+
+printCentroids :: [Cluster] -> [Point]
+printCentroids = map centroid
 
 parseFile :: Maybe Options -> IO ()
 parseFile Nothing = print "Error: Failed to parse options"
 parseFile (Just opt) = do
-    content <- readFile (file opt)
-    putStrLn content
+  content <- readFile (file opt)
+
+  putStrLn (show (nbrColors opt))
+  putStrLn (show (convergence opt))
+  -- putStrLn content
+
+  let li = lines content
+  clusters <- startCluster li (maybeToInt (nbrColors opt))
+  let allp = strToPoint li
+  print clusters
+
+  let res = kMeans allp clusters (maybeToFloat (convergence opt))
+  let centroids = printCentroids res
+  mapM_ print centroids
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let options = parseOptions args
-    -- print options
-    case options of
-        Just o -> do
-            if convergence o == Nothing then
-                print "Error: convergence is not set"
-            else if nbrColors o == Nothing then
-                print "Error: nbrColors is not set"
-            else if file o == "" then
-                print "Error: file is not set"
+  args <- getArgs
+  let options = parseOptions args
+  -- print options
+  case options of
+    Just o -> do
+      if convergence o == Nothing
+        then print "Error: convergence is not set"
+        else
+          if nbrColors o == Nothing
+            then print "Error: nbrColors is not set"
             else
-                parseFile options
-
-        Nothing -> print "Error: Failed to parse options"
+              if file o == ""
+                then print "Error: file is not set"
+                else parseFile options
+    Nothing -> print "Error: Failed to parse options"
 
 -- main :: IO ()
 -- main = do
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,11 +4,186 @@
 -- File description:
 -- Lib.hs
 -}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Lib
-    ( someFunc
-    ) where
+  ( -- charToInt,
+    distance,
+    -- ok,
+    -- closest,
+    startCluster,
+    Point (..),
+    Cluster (..),
+    kMeans,
+    strToPoint,
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad (replicateM)
+-- import Data.Char (isDigit, ord)
+import Data.Function (on)
+-- import Data.List (transpose)
+import System.Random (randomRIO)
 
+data Point = Point
+  { r :: Int,
+    g :: Int,
+    b :: Int,
+    posx :: Int,
+    posy :: Int
+  }
+  deriving (Show)
+
+-- instance Num Point where
+--   (+) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 + r2) (g1 + g2) (b1 + b2)
+--   (*) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 * r2) (g1 * g2) (b1 * b2)
+--   (-) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 - r2) (g1 - g2) (b1 - b2)
+--   abs (Point r1 g1 b1) = Point (abs r1) (abs g1) (abs b1)
+--   signum (Point r1 g1 b1) = Point (signum r1) (signum g1) (signum b1)
+--   fromInteger i = Point (fromInteger i) (fromInteger i) (fromInteger i)
+
+-- instance Fractional Point where
+--   (/) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 `div` r2) (g1 `div` g2) (b1 `div` b2)
+--   fromRational r = Point (fromIntegral $ round r) (fromIntegral $ round r) (fromIntegral $ round r)
+
+instance Eq Point where
+  (==) = (==) `on` (\(Point r g b posx posy) -> (r, g, b, posx, posy))
+
+-- instance Ord Point where
+--   compare = compare `on` (\(Point r g b) -> (r, g, b))
+
+data Cluster = Cluster
+  { centroid :: Point,
+    points :: [Point]
+  }
+  deriving (Show)
+
+instance Eq Cluster where
+  c1 == c2 = centroid c1 == centroid c2 && points c1 == points c2
+
+-- charToInt :: Char -> Float
+-- charToInt c = fromIntegral (ord c - ord '0')
+distance :: Point -> Point -> Float
+distance point1 point2 =
+  sqrt
+    ( fromIntegral
+        ( (r point1 - r point2) ^ 2
+            + (g point1 - g point2) ^ 2
+            + (b point1 - b point2) ^ 2
+        )
+    )
+
+-- ok :: String -> [Float]
+-- ok [] = []
+-- ok (x : xs)
+--   | x <= '9' && x >= '0' = charToInt x : ok xs
+--   | otherwise = ok xs
+
+replace :: Eq a => [a] -> [a] -> [a] -> [a]
+replace _ _ [] = []
+replace find replaceWith (x : xs)
+  | x == head find = head replaceWith : replace find replaceWith xs
+  | otherwise = x : replace find replaceWith xs
+
+parseTuple :: String -> [Int]
+parseTuple s = map read $ filter (not . null) $ splitByComma (replace ")" " " $ replace "(" " " s)
+  where
+    splitByComma = foldr (\c acc -> if c == ',' then [] : acc else (c : head acc) : tail acc) [[]]
+
+-- closest :: [Point] -> Point -> Point -> Int
+-- closest [] _ z = z
+-- closest (x : xs) y z
+--   | distance (ok x) y < distance (ok z) y = closest xs y x
+--   | otherwise = closest xs y z
+
+strToCluster :: String -> Cluster
+strToCluster str = Cluster {centroid = Point (head (parseTuple pint)) (parseTuple pint !! 1) (parseTuple pint !! 2) (head (parseTuple f)) (last (parseTuple f)), points = []}
+  where
+    pint = last (words str)
+    f = head (words str)
+
+creatCluster :: [String] -> Int -> IO [Cluster]
+creatCluster _ 0 = return []
+creatCluster x k = do
+  indices <- replicateM k $ randomRIO (0, length x - 1)
+  return $ map (strToCluster . (x !!)) indices
+
+startCluster :: [String] -> Int -> IO [Cluster]
+startCluster li k = print (strToCluster (head li)) >> creatCluster li k
+
+strToPoint :: [String] -> [Point]
+strToPoint [] = []
+strToPoint (x : xs) = Point (head (parseTuple pint)) (parseTuple pint !! 1) (parseTuple pint !! 2) (head (parseTuple f)) (last (parseTuple f)) : strToPoint xs
+  where
+    pint = last (words x)
+    f = head (words x)
+
+getMin :: Point -> [Cluster] -> Float -> Int -> Int -> Int
+getMin _ [] _ _ res = res
+getMin x (y : ys) mins i res
+  | distance x (centroid y) < mins = getMin x ys (distance x (centroid y)) (i + 1) i
+  | otherwise = getMin x ys mins (i + 1) res
+
+-- meanPoint :: [Point] -> Point
+-- meanPoint ps = sum ps / fromIntegral (length ps)
+
+-- updCluster :: [Cluster] -> [Point] -> [Cluster]
+-- updCluster [] _ = []
+-- updCluster (x : xs) cluster
+--   | not (null (points x)) = Cluster {centroid = meanPoint (points x), points = []} : updCluster xs cluster
+--   | otherwise = x : updCluster xs cluster
+
+-- updPoint :: [Cluster] -> [Point] -> [Cluster]
+-- updPoint cluster [] = cluster
+-- updPoint cluster (x : xs) = do
+--   let newCluster = cluster !! (getMin (centroid x) cluster (distance x (head cluster)) 0) {points = x : points}
+--   updPoint cluster xs
+-- updPoint :: [Cluster] -> [Point] -> [Cluster]
+-- updPoint = foldr updatePoint
+--   where
+--     updatePoint :: Point -> [Cluster] -> [Cluster]
+--     updatePoint point [] = [Cluster point [point]]
+--     updatePoint point (cluster : rest) =
+--       let closestCluster = foldr (\c1 c2 -> if distance (centroid c1) point < distance (centroid c2) point then c1 else c2) cluster rest
+--        in closestCluster {points = point : points closestCluster} : filter (/= closestCluster) rest
+
+convergence :: [Cluster] -> [Cluster] -> Float -> Bool
+convergence [] [] _ = True
+convergence (c1 : cs1) (c2 : cs2) threshold
+  | distance (centroid c1) (centroid c2) <= threshold = convergence cs1 cs2 threshold
+  | otherwise = False
+
+-- kMeans :: [Cluster] -> [Point] -> Float -> [Cluster]
+-- kMeans cluster points c = do
+--   let newCluster = updCluster cluster points
+--   let newPoint = updPoint newCluster points
+--   if convergence newCluster cluster c
+--     then newPoint
+--     else kMeans newPoint points c
+
+getNewPos :: Cluster -> [Point] -> Int -> Int -> Int -> Int -> Point
+getNewPos c [] sumR sumG sumB count
+  | count == 0 = (centroid c) {r = sumR, g = sumG, b = sumB}
+  | otherwise = (centroid c) {r = sumR `div` count, g = sumG `div` count, b = sumB `div` count}
+getNewPos c (x : xs) sumR sumG sumB count = getNewPos c xs (sumR + r x) (sumG + g x) (sumB + b x) (count + 1)
+
+calcNew :: [Cluster] -> [Cluster]
+calcNew [] = []
+calcNew (x : xs) = x {centroid = getNewPos x (points x) 0 0 0 0, points = []} : calcNew xs
+
+-- orgNew :: [Point] -> [Cluster] -> [Cluster]
+-- orgNew [] cluster = cluster
+-- orgNew (x : xs) cluster = orgNew xs (cluster !! (getMin x cluster 1000 0 0) {points = x : points})
+
+orgNew :: [Point] -> [Cluster] -> [Cluster]
+orgNew [] clusters = clusters
+orgNew (x : xs) clusters =
+  let closestCluster = foldr (\c1 c2 -> if distance (centroid c1) x < distance (centroid c2) x then c1 else c2) (head clusters) clusters
+      updatedCluster = closestCluster {points = x : points closestCluster}
+      restClusters = filter (/= closestCluster) clusters
+   in orgNew xs (updatedCluster : restClusters)
+
+kMeans :: [Point] -> [Cluster] -> Float -> [Cluster]
+kMeans allp clusters threshold
+  | convergence (calcNew clusters) clusters threshold = orgNew allp (calcNew clusters)
+  | otherwise = kMeans allp (orgNew allp (calcNew clusters)) threshold
