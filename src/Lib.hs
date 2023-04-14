2d95ee0293e7c13af0b44943e7a067ef4f873e1f
EwenBriand <ewen.briand@epitech.eu>
2023-04-14 11:58:32 +0200

    [FIX] better random... i hope

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,7 +23,7 @@ import Control.Monad (replicateM)
 -- import Data.Char (isDigit, ord)
 import Data.Function (on)
 -- import Data.List (transpose)
-import System.Random (randomRIO)
+import System.Random
 
 data Point = Point
   { r :: Int,
@@ -34,23 +34,8 @@ data Point = Point
   }
   deriving (Show)
 
--- instance Num Point where
---   (+) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 + r2) (g1 + g2) (b1 + b2)
---   (*) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 * r2) (g1 * g2) (b1 * b2)
---   (-) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 - r2) (g1 - g2) (b1 - b2)
---   abs (Point r1 g1 b1) = Point (abs r1) (abs g1) (abs b1)
---   signum (Point r1 g1 b1) = Point (signum r1) (signum g1) (signum b1)
---   fromInteger i = Point (fromInteger i) (fromInteger i) (fromInteger i)
-
--- instance Fractional Point where
---   (/) (Point r1 g1 b1) (Point r2 g2 b2) = Point (r1 `div` r2) (g1 `div` g2) (b1 `div` b2)
---   fromRational r = Point (fromIntegral $ round r) (fromIntegral $ round r) (fromIntegral $ round r)
-
 instance Eq Point where
   (==) = (==) `on` (\(Point r g b posx posy) -> (r, g, b, posx, posy))
-
--- instance Ord Point where
---   compare = compare `on` (\(Point r g b) -> (r, g, b))
 
 data Cluster = Cluster
   { centroid :: Point,
@@ -61,8 +46,6 @@ data Cluster = Cluster
 instance Eq Cluster where
   c1 == c2 = centroid c1 == centroid c2 && points c1 == points c2
 
--- charToInt :: Char -> Float
--- charToInt c = fromIntegral (ord c - ord '0')
 distance :: Point -> Point -> Float
 distance point1 point2 =
   sqrt
@@ -72,12 +55,6 @@ distance point1 point2 =
             + (b point1 - b point2) ^ 2
         )
     )
-
--- ok :: String -> [Float]
--- ok [] = []
--- ok (x : xs)
---   | x <= '9' && x >= '0' = charToInt x : ok xs
---   | otherwise = ok xs
 
 replace :: Eq a => [a] -> [a] -> [a] -> [a]
 replace _ _ [] = []
@@ -89,12 +66,6 @@ parseTuple :: String -> [Int]
 parseTuple s = map read $ filter (not . null) $ splitByComma (replace ")" " " $ replace "(" " " s)
   where
     splitByComma = foldr (\c acc -> if c == ',' then [] : acc else (c : head acc) : tail acc) [[]]
-
--- closest :: [Point] -> Point -> Point -> Int
--- closest [] _ z = z
--- closest (x : xs) y z
---   | distance (ok x) y < distance (ok z) y = closest xs y x
---   | otherwise = closest xs y z
 
 strToCluster :: String -> Cluster
 strToCluster str = Cluster {centroid = poin, points = []}
@@ -109,12 +80,26 @@ checkIndices (x : xs)
   | x `elem` xs = False
   | otherwise = checkIndices xs
 
+-- getRdmIdc :: [String] -> Int -> IO [Int]
+-- getRdmIdc x k = do
+--   indices <- replicateM k $ randomRIO (0, length x - 1)
+--   if checkIndices indices
+--     then return indices
+--     else getRdmIdc x k
+
 getRdmIdc :: [String] -> Int -> IO [Int]
 getRdmIdc x k = do
-  indices <- replicateM k $ randomRIO (0, length x - 1)
+  gen <- newStdGen -- get new random generator based on current time
+  let indices = take k $ randomRs (0, length x - 1) gen -- generate k random indices using randomRs
   if checkIndices indices
     then return indices
     else getRdmIdc x k
+
+-- newStdGenWithTime :: IO StdGen
+-- newStdGenWithTime = do
+--   t <- getCurrentTime
+--   let (s1, s2) = split $ mkStdGen $ fromIntegral $ utctDayTime t
+--   return s2
 
 -- checkIndice :: [Int] -> Int -> Bool
 -- checkIndice [] _ = True
@@ -177,48 +162,11 @@ strToPoint (x : xs) = Point (head (parseTuple pint)) (parseTuple pint !! 1) (par
     pint = last (words x)
     f = head (words x)
 
-getMin :: Point -> [Cluster] -> Float -> Int -> Int -> Int
-getMin _ [] _ _ res = res
-getMin x (y : ys) mins i res
-  | distance x (centroid y) < mins = getMin x ys (distance x (centroid y)) (i + 1) i
-  | otherwise = getMin x ys mins (i + 1) res
-
--- meanPoint :: [Point] -> Point
--- meanPoint ps = sum ps / fromIntegral (length ps)
-
--- updCluster :: [Cluster] -> [Point] -> [Cluster]
--- updCluster [] _ = []
--- updCluster (x : xs) cluster
---   | not (null (points x)) = Cluster {centroid = meanPoint (points x), points = []} : updCluster xs cluster
---   | otherwise = x : updCluster xs cluster
-
--- updPoint :: [Cluster] -> [Point] -> [Cluster]
--- updPoint cluster [] = cluster
--- updPoint cluster (x : xs) = do
---   let newCluster = cluster !! (getMin (centroid x) cluster (distance x (head cluster)) 0) {points = x : points}
---   updPoint cluster xs
--- updPoint :: [Cluster] -> [Point] -> [Cluster]
--- updPoint = foldr updatePoint
---   where
---     updatePoint :: Point -> [Cluster] -> [Cluster]
---     updatePoint point [] = [Cluster point [point]]
---     updatePoint point (cluster : rest) =
---       let closestCluster = foldr (\c1 c2 -> if distance (centroid c1) point < distance (centroid c2) point then c1 else c2) cluster rest
---        in closestCluster {points = point : points closestCluster} : filter (/= closestCluster) rest
-
 convergence :: [Cluster] -> [Cluster] -> Float -> Bool
 convergence [] [] _ = True
 convergence (c1 : cs1) (c2 : cs2) threshold
   | distance (centroid c1) (centroid c2) <= threshold = convergence cs1 cs2 threshold
   | otherwise = False
-
--- kMeans :: [Cluster] -> [Point] -> Float -> [Cluster]
--- kMeans cluster points c = do
---   let newCluster = updCluster cluster points
---   let newPoint = updPoint newCluster points
---   if convergence newCluster cluster c
---     then newPoint
---     else kMeans newPoint points c
 
 getNewPos :: Cluster -> [Point] -> Int -> Int -> Int -> Int -> Point
 getNewPos c [] sumR sumG sumB count
@@ -230,10 +178,6 @@ calcNew :: [Cluster] -> [Cluster]
 calcNew =
   map
     (\x -> x {centroid = getNewPos x (points x) 0 0 0 0, points = []})
-
--- orgNew :: [Point] -> [Cluster] -> [Cluster]
--- orgNew [] cluster = cluster
--- orgNew (x : xs) cluster = orgNew xs (cluster !! (getMin x cluster 1000 0 0) {points = x : points})
 
 closest :: Point -> [Cluster] -> Cluster -> Cluster
 closest _ [] best = best
